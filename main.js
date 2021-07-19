15aa140b573393aae77a4023fbef42036702ed12
codnjs779 <76987275+codnjs779@users.noreply.github.com>
2021-07-19 15:20:52 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 // 구현해 낼 것: 커서가 마우스를 따라 움직이고 가로 세로 선이 있는 화면 
-// 
+const hori = document.querySelector('.hori');
+const verti = document.querySelector('.verti');
+const cursorItem = document.querySelector('.cursorItem img');
+const num = document.querySelector('.num');
 
-const cursor = document.querySelector('.cursorItem');
-const h3 = document.querySelector('h3');
-const html = document.querySelector('html');
-const horizontal = document.querySelector('.horizontal');
-const align = document.querySelector('.align');
+// 이전 코드에서 mouseover event를 사용했는데 그렇게 되면 뻑뻑(?)하게 움직이는 느낌이 계속 났다 
+//mouseover은 요소에 마우스가 들어갔다 나올 때 마다 이벤트가 실행되는 것이고 mousemove는 마우스를 움직일때 이벤트가 실행되는 것이기 때문에 차이가 났던것!! 
+ 
 
-function mouseFunc(event) {
-    html.getBoundingClientRect();
-    let x= event.clientX;
-    let y= event.clientY;
-    h3.innerHTML = `${x}, ${y}`;
-    cursor.style.transform = `translate(${x}px, ${y}px)`;
-    h3.style.transform = `translate(${x}px, ${y}px)`;
-    horizontal.style.transform = `translate(${x}px, ${y}px)`;
-    align.style.transform = `translate(${x}px, ${y}px)`;
-}
+document.addEventListener('mousemove', (etn) => {
+    const x = etn.clientX;
+    const y = etn.clientY;
+    num.innerHTML = `${x} , ${y}`;
+    num.style.left = `${x}px`;
+    num.style.top = `${y}px`;
 
-window.addEventListener('load' , () => {
-    html.addEventListener('mouseover', mouseFunc);
-});
-//d아직 defer 문제 해결하지 못함!! 내일 해결하고 다시 커밋 
+    verti.style.left = `${x}px`;
+    hori.style.top = `${y}px`;
 
-
+    cursorItem.style.left = `${x}px`;
+    cursorItem.style.top = `${y}px`;
+    
+    
+})
