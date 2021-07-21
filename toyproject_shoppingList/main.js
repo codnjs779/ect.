572b0cfe0749c873bf8b1d4c4fb1f0c73d13d2c1
codnjs779 <76987275+codnjs779@users.noreply.github.com>
2021-07-21 23:52:28 +0900

    create addList
    create addList but li is not apply. a solution is needed.

---
+++ toyproject_shoppingList/main.js
@@ -0,0 +1,15 @@
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
+const input = document.querySelector('form input');
+const btn = document.querySelector('form button');
+
+btn.addEventListener('click', listAdd);
+
+function listAdd() {
+    let newList = document.createElement('li');
+    let newContent = input.value;
+    newList.textContent = newContent;
+    ul.appendChild(newList);
+    console.log(newList);
+}
+
